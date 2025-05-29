5f7b5a66d72dbd7d4017d580e0f638d56bca81b9
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-29 17:14:55 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -38,7 +38,7 @@ const connect = async () => {
 
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5175"],
+  origin: ["http://localhost:5173", "http://localhost:5175","https://skillable-freelancer.onrender.com"],
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
