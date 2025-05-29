a51d4a9f77d5ba19bd389db1db4ebe2e8b4c2f08
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-30 00:50:16 +0530

    Update savedGig.route.js

--- api/routes/savedGig.route.js
+++ api/routes/savedGig.route.js
@@ -11,14 +11,12 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.send("Saved gigs route is working!");
 });
+
 // Save or unsave a gig
 router.put("/toggle/:id", verifyToken, toggleSaveGig);
 
 // Get all saved gigs for the current user
 router.get("/", verifyToken, getSavedGigs);
-
-
-
 
 // Check if a gig is saved by the current user
 router.get("/check/:id", verifyToken, checkSavedGig);
