8f596af5aed9e5d5ed23c584068bb746439c669d
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-30 00:34:52 +0530

    Update savedGig.route.js

--- api/routes/savedGig.route.js
+++ api/routes/savedGig.route.js
@@ -8,15 +8,16 @@ import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.send("Saved gigs route is working!");
+});
 // Save or unsave a gig
 router.put("/toggle/:id", verifyToken, toggleSaveGig);
 
 // Get all saved gigs for the current user
 router.get("/", verifyToken, getSavedGigs);
 
-router.get("/test", (req, res) => {
-  res.send("Saved gigs route is working!");
-});
+
 
 
 // Check if a gig is saved by the current user
