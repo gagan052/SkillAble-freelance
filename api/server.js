b4c765c4d2b02e1a389fbd3f5d4cf480dfcdf107
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-30 16:17:23 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -41,8 +41,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
      "http://localhost:5174",
-     "https://skillable-freelancer.onrender.com/api/",
-     "https://new-skillable-frontend.onrender.com/api/",
+     "https://skillable-freelancer.onrender.com",
+     "https://new-skillable-frontend.onrender.com",
     ],
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   credentials: true,
