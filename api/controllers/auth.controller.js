e24746094d0ca2071374723a3e6658e228672cbb
gagan052 <147720611+gagan052@users.noreply.github.com>
2025-05-30 15:56:22 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -39,8 +39,8 @@ export const login = async (req, res, next) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
-        sameSite: "none",
-        secure: true,
+        sameSite: "lax",
+        secure: false,
       })
       .status(200)
       .send({ ...info, accessToken: token });
